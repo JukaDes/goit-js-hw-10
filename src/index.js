870a787dd4a9d6b804adf159094077c88f376d69
Jekaterina Sedjuka <jekaterina.sedjuka@gmail.com>
2022-10-24 05:10:10 +0100

    finished hw10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
-import debounce from 'lodash.debounce';
-
-const DEBOUNCE_DELAY = 300;
+import { fetchCountries } from "./js/fetchCountries";
